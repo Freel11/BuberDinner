c052cea88db58dc766f72266d75a20a88f33bc9b
Freel11 <jadefreeljd@gmail.com>
2025-05-01 17:19:54 -0400

    Added a basic authentication controller with register and login requests

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,10 +1,21 @@
+using BuberDinner.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
 
 [ApiController]
-[Route("api/auth")]
+[Route("auth")]
 public class AuthenticationController : ControllerBase
 {
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest registerRequest)
+    {
+        return Ok(registerRequest);
+    }
 
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest loginRequest)
+    {
+        return Ok(loginRequest);
+    }
 }
