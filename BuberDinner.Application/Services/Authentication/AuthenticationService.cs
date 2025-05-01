ffe440be4b9b0304da160ab78437304c64af493a
Freel11 <jadefreeljd@gmail.com>
2025-05-01 18:56:13 -0400

    Added interface definition for the authentication service

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,1 +1,14 @@
 namespace BuberDinner.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
