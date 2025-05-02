5a8afe04f1ad76b129c2f8fe2aaabdd05f319bd3
Freel11 <jadefreeljd@gmail.com>
2025-05-02 13:59:59 -0400

    Introduct ErrorOr package for flow control

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Contracts.Authentication;
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
@@ -11,20 +12,26 @@ public class AuthenticationController(IAuthenticationService authenticationServi
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest registerRequest)
     {
-        var authResult = authenticationService.Register(
+        ErrorOr<AuthenticationResult> authResult = authenticationService.Register(
             registerRequest.FirstName,
             registerRequest.LastName,
             registerRequest.Email,
             registerRequest.Password);
 
-        var response = new AuthenticationResponse(
+        return authResult.Match(
+            authResult => Ok(MapAuthResponse(authResult)),
+            _ => Problem(statusCode: StatusCodes.Status409Conflict, title: "Email already in use")
+        );
+    }
+
+    private static AuthenticationResponse MapAuthResponse(AuthenticationResult authResult)
+    {
+        return new AuthenticationResponse(
             authResult.User.Id,
             authResult.User.FirstName,
             authResult.User.LastName,
             authResult.User.Email,
             authResult.Token);
-
-        return Ok(response);
     }
 
     [HttpPost("login")]
--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,6 +1,8 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Common.Errors;
 using BuberDinner.Domain.Entities;
+using ErrorOr;
 
 namespace BuberDinner.Application.Services.Authentication;
 
@@ -9,12 +11,12 @@ public class AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRe
     private readonly IJwtTokenGenerator _jwtTokenGenerator = jwtTokenGenerator;
     private readonly IUserRepository _userRepository = userRepository;
 
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    public ErrorOr<AuthenticationResult> Register(string firstName, string lastName, string email, string password)
     {
         // 1. Validate the user doesn't exist
         if (_userRepository.GetUserByEmail(email) is not null)
         {
-            throw new Exception("User with given email already exists");
+            return Errors.User.DuplicateEmail;
         }
 
         // 2. Create a new user (generate unique ID) & Persist to db
