e7ef9d7aa96b75585c61ac9f797313a7c796b07c
Freel11 <jadefreeljd@gmail.com>
2025-05-02 13:03:26 -0400

    Introduced global error handling via problem details

---
+++ BuberDinner.Api/Errors/BuberDinnerProblemDetailsFactory.cs
@@ -0,0 +1,1 @@
+namespace BuberDinner.Api.Errors;
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -6,12 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-        
+
+    builder.Services.AddProblemDetails(options =>
+    {
+        options.CustomizeProblemDetails = (context) =>
+        {
+            context.ProblemDetails.Extensions.Add("Custom Property", "Custom Value");
+        };
+    });
+
     builder.Services.AddControllers();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 
